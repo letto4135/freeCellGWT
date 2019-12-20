2bac9d03c8bdbb1784706a3e8530b506f10af103
letto4135 <35670693+letto4135@users.noreply.github.com>
2019-12-20 14:10:21 -0500

    final commit before public repository

---
+++ electronApp/FreeCellFinalExe-win32-x64/resources/app/main.js
@@ -0,0 +1,91 @@
+// Modules to control application life and create native browser window
+const {app, BrowserWindow, screen, Menu, MenuItem} = require('electron')
+const path = require('path')
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow
+let menu = new Menu()
+
+function createWindow () {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  const template = [
+    // { role: 'fileMenu' }
+    {
+      label: 'File',
+      submenu: [
+        { role: 'reload', label: 'New Game'},
+        { role: 'quit' }
+      ]
+    },
+    // { role: 'viewMenu' }
+    {
+      label: 'View',
+      submenu: [
+        { role: 'zoomin' },
+        { role: 'zoomout' },
+        { type: 'separator' },
+        { role: 'togglefullscreen' }
+      ]
+    },
+    // { role: 'windowMenu' }
+    {
+      label: 'Window',
+      submenu: [
+        { role: 'minimize' },
+        ...([
+          { role: 'close' }
+        ])
+      ]
+    },
+    {
+      role: 'help',
+      submenu: [
+        { label: 'Learn More (external)',
+            click: async () => {
+              const { shell } = require('electron')
+              await shell.openExternal('https://electronjs.org')
+            }
+        },
+        { label: 'FreeCell Rules (external)',
+            click: async () => {
+              const { shell } = require('electron')
+              await shell.openExternal('http://www.solitairecity.com/FreeCell.shtml')
+            }
+        }
+      ]
+    }
+  ]
+  
+  const menu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(menu)
+
+  // and load the index.html of the app.
+  mainWindow.loadFile('index.html')
+  mainWindow.maximize()
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null
+  })
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
---
+++ webApp/src/tests/ModelTests.java
@@ -0,0 +1,41 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import life.model.FreeCellModel;
+
+class ModelTests {
+	@Test
+	void test() {
+		FreeCellModel model = new FreeCellModel();
+		
+		System.out.println("Testing FreeCellModel class");
+		model.StartGame();
+		System.out.println("Number of freecells should be 4, answer : " + model.freecells.size());
+		System.out.println("Number of foundations should be 4, answer : " + model.foundations.size());
+		System.out.println("Number of columns should be 8, answer : " + model.columns.size());
+		assertTrue(model.freecells.size() == 4);
+		assertTrue(model.foundations.size() == 4);
+		assertTrue(model.columns.size() == 8);
+		
+		System.out.println("Number of cards in first 4 columns should be 7, answer: ");
+		System.out.println(model.columns.get(0).size());
+		System.out.println(model.columns.get(1).size());
+		System.out.println(model.columns.get(2).size());
+		System.out.println(model.columns.get(3).size());
+		assertTrue(model.columns.get(0).size() == 7);
+		assertTrue(model.columns.get(1).size() == 7);
+		assertTrue(model.columns.get(2).size() == 7);
+		assertTrue(model.columns.get(3).size() == 7);
+		System.out.println("Number of cards in last 4 columns should be 6, answer: ");
+		System.out.println(model.columns.get(4).size());
+		System.out.println(model.columns.get(5).size());
+		System.out.println(model.columns.get(6).size());
+		System.out.println(model.columns.get(7).size());
+		assertTrue(model.columns.get(4).size() == 6);
+		assertTrue(model.columns.get(5).size() == 6);
+		assertTrue(model.columns.get(6).size() == 6);
+		assertTrue(model.columns.get(7).size() == 6);
+	}
+}
